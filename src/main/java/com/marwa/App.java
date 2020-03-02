894f9e9a48a0707fa4347cc9ca843976d696e08a
marwa-altom <marwa@quickschools.com>
2020-03-02 16:19:06 +0800

    this is first commit

---
+++ src/main/java/com/marwa/App.java
@@ -0,0 +1,13 @@
+package com.marwa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!, I am marwa." );
+    }
+}
